d63ed8d429dec28a241d80d48bb9c736b98c1ced
Youri Bogaars <Youri Bogaars@DESKTOP-G5TAT80>
2017-11-09 15:15:38 +0100

    save to gallery

--- app/src/main/java/com/s21m/proftaaks2maatwerk/ui/PictureTakenActivity.java
+++ app/src/main/java/com/s21m/proftaaks2maatwerk/ui/PictureTakenActivity.java
@@ -3,16 +3,29 @@ package com.s21m.proftaaks2maatwerk.ui;
 import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
+import android.media.Image;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.s21m.proftaaks2maatwerk.R;
 
+import org.w3c.dom.Text;
+
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
@@ -22,105 +35,70 @@ public class PictureTakenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture_taken);
+
     }
 
-    public static final String insertImage(ContentResolver cr,
-                                           Bitmap source,
-                                           String title,
-                                           String description) {
+    public void SavePicture(View view) {
+        ImageView imageView = (ImageView) findViewById(R.id.imageViewPicture);
+        TextView textView = (TextView) findViewById(R.id.textViewTitle);
+        imageView.buildDrawingCache();
+        Bitmap picture = imageView.getDrawingCache();
+        String pictureName = textView.getText().toString();
+        PictureToGallery(pictureName, picture);
 
-        ContentValues values = new ContentValues();
-        values.put(MediaStore.Images.Media.TITLE, title);
-        values.put(MediaStore.Images.Media.DISPLAY_NAME, title);
-        values.put(MediaStore.Images.Media.DESCRIPTION, description);
-        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
-        // Add the date meta data to ensure the image is added at the front of the gallery
-        values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis() / 1000);
-        values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
 
-        Uri url = null;
-        String stringUrl = null;    /* value to be returned */
+    }
 
+    public boolean PictureToGallery(String pictureName, Bitmap picture) {
+
+        // TODO Auto-generated method stub
+
+
+        OutputStream output;
+
+        // Retrieve the image from the res folder
+
+        // Find the SD Card path
+        File filepath = Environment.getExternalStorageDirectory();
+
+        // Create a new folder in SD Card
+        File dir = new File(filepath.getAbsolutePath()
+                + "/Images Classifier/");
+        dir.mkdirs();
+
+        // Create a name for the saved image
+        File file = new File(dir, pictureName);
+
+        // Show a toast message on successful save
+        Toast.makeText(this, "Image Saved to SD Card",
+                Toast.LENGTH_SHORT).show();
         try {
-            url = cr.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
 
-            if (source != null) {
-                OutputStream imageOut = cr.openOutputStream(url);
-                try {
-                    source.compress(Bitmap.CompressFormat.JPEG, 50, imageOut);
-                } finally {
-                    imageOut.close();
-                }
+            output = new FileOutputStream(file);
 
-                long id = ContentUris.parseId(url);
-                // Wait until MINI_KIND thumbnail is generated.
-                Bitmap miniThumb = MediaStore.Images.Thumbnails.getThumbnail(cr, id, MediaStore.Images.Thumbnails.MINI_KIND, null);
-                // This is for backward compatibility.
-                storeThumbnail(cr, miniThumb, id, 50F, 50F, MediaStore.Images.Thumbnails.MICRO_KIND);
-            } else {
-                cr.delete(url, null, null);
-                url = null;
-            }
-        } catch (Exception e) {
-            if (url != null) {
-                cr.delete(url, null, null);
-                url = null;
-            }
+            // Compress into png format image from 0% - 100%
+            picture.compress(Bitmap.CompressFormat.PNG, 100, output);
+            output.flush();
+            output.close();
+            return true;
         }
 
-        if (url != null) {
-            stringUrl = url.toString();
+        catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return false;
         }
 
-        return stringUrl;
     }
 
-    /**
-     * A copy of the Android internals StoreThumbnail method, it used with the insertImage to
-     * populate the android.provider.MediaStore.Images.Media#insertImage with all the correct
-     * meta data. The StoreThumbnail method is private so it must be duplicated here.
-     * @see android.provider.MediaStore.Images.Media (StoreThumbnail private method)
-     */
-    private static final Bitmap storeThumbnail(
-            ContentResolver cr,
-            Bitmap source,
-            long id,
-            float width,
-            float height,
-            int kind) {
 
-        // create the matrix to scale it
-        Matrix matrix = new Matrix();
 
-        float scaleX = width / source.getWidth();
-        float scaleY = height / source.getHeight();
 
-        matrix.setScale(scaleX, scaleY);
-
-        Bitmap thumb = Bitmap.createBitmap(source, 0, 0,
-                source.getWidth(),
-                source.getHeight(), matrix,
-                true
-        );
-
-        ContentValues values = new ContentValues(4);
-        values.put(MediaStore.Images.Thumbnails.KIND,kind);
-        values.put(MediaStore.Images.Thumbnails.IMAGE_ID,(int)id);
-        values.put(MediaStore.Images.Thumbnails.HEIGHT,thumb.getHeight());
-        values.put(MediaStore.Images.Thumbnails.WIDTH,thumb.getWidth());
-
-        Uri url = cr.insert(MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI, values);
-
-        try {
-            OutputStream thumbOut = cr.openOutputStream(url);
-            thumb.compress(Bitmap.CompressFormat.JPEG, 100, thumbOut);
-            thumbOut.close();
-            return thumb;
-        } catch (FileNotFoundException ex) {
-            return null;
-        } catch (IOException ex) {
-            return null;
-        }
-    }
 }
+
+
+
+
+
+
 
